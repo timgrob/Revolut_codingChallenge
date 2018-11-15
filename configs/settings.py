6b98ffac88c0bd60565319095660df208728cc34
Tim Grob <tim.grob@swissonline.ch>
2018-11-15 22:55:09 +0100

    Update settings.py
    Anonymise database credentials.

--- configs/settings.py
+++ configs/settings.py
@@ -1,5 +1,5 @@
-DATABASE_USER = 'timgrob'
-DATABASE_PW = ''
-DATABASE_NAME = 'revolut_db'
-DATABASE_PORT = '5432'
+DATABASE_USER = 'db_user'
+DATABASE_PW = 'db_pw'
+DATABASE_NAME = 'db_name'
+DATABASE_PORT = 'db_port'
 DATABASE_URL = 'postgresql+psycopg2://{}:{}@localhost:{}/{}'.format(DATABASE_USER,DATABASE_PW,DATABASE_PORT,DATABASE_NAME)
